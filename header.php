320ab7cc5a2b192b386f62bcfc71718f46ed46b5
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2024-10-06 23:05:07 +0600

    working on header.php

--- header.php
+++ header.php
@@ -25,35 +25,36 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'clark' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$clark_description = get_bloginfo( 'description', 'display' );
-			if ( $clark_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $clark_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'clark' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar ftco-navbar-light site-navbar-target" id="ftco-navbar">
+			<div class="container">
+				<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+				<button class="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle" type="button" data-toggle="collapse"
+					data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="oi oi-menu"></span> Menu
+				</button>
+	
+				<div class="collapse navbar-collapse" id="ftco-nav">
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_class'        => 'navbar-nav nav ml-auto',
+						)
+					);
+					?>
+					<!-- <ul class="navbar-nav nav ml-auto">
+						<li class="nav-item"><a href="#home-section" class="nav-link"><span>Home</span></a></li>
+						<li class="nav-item"><a href="#about-section" class="nav-link"><span>About</span></a></li>
+						<li class="nav-item"><a href="#resume-section" class="nav-link"><span>Resume</span></a></li>
+						<li class="nav-item"><a href="#services-section" class="nav-link"><span>Services</span></a></li>
+						<li class="nav-item"><a href="#skills-section" class="nav-link"><span>Skills</span></a></li>
+						<li class="nav-item"><a href="#projects-section" class="nav-link"><span>Projects</span></a></li>
+						<li class="nav-item"><a href="#blog-section" class="nav-link"><span>My Blog</span></a></li>
+						<li class="nav-item"><a href="#contact-section" class="nav-link"><span>Contact</span></a></li>
+					</ul> -->
+				</div>
+			</div>
+		</nav>
+	</header>
